ab8d6bc3f466dd26ac25821d3a2101cbe9a1bb63
Murad501 <mm845635@gmail.com>
2023-11-01 17:09:04 +0600

    [Done] all curriculum borad page

--- src/pages/CurriculumBoard/AllCurriculumBoard/AllCurriculumBoard.jsx
+++ src/pages/CurriculumBoard/AllCurriculumBoard/AllCurriculumBoard.jsx
@@ -4,7 +4,6 @@ import toast from "react-hot-toast";
 import { curriculumBoards } from "../../../data/curriculumBoards";
 import { educationVariants } from "../../../data/educationVariant";
 
-
 const AllCurriculumBoard = () => {
   return (
     <div className="py-10">
@@ -18,46 +17,59 @@ const AllCurriculumBoard = () => {
         </Link>
       </div>
       <div className="overflow-x-auto">
-        <table className="table">
+        <table className="w-full min-w-max table-auto text-left">
           {/* head */}
           <thead>
             <tr>
-              <th>Sl</th>
-              <th>Name</th>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                Sl
+              </th>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                Name
+              </th>
 
-              <th>Variant</th>
-              <th className="w-[120px] text-center">Actions</th>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                Variant
+              </th>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4 w-[120px] text-center">
+                Actions
+              </th>
             </tr>
           </thead>
           <tbody>
             {/* row 1 */}
-            {curriculumBoards.map((board, idx) => (
-              <tr key={board.id} className="hover">
-                <th>{idx + 1}</th>
-                <td>{board.name}</td>
-                <td>
-                  {
-                    educationVariants.find(
-                      (variant) => variant.id === board.mediumId
-                    ).name
-                  }
-                </td>
-                <td className="w-[120px] flex justify-evenly items-center">
-                  <FaTrash
-                    onClick={() =>
-                      toast.success("Curriculum Board deleted successfully")
+            {curriculumBoards.map((board, idx) => {
+              const classes = "p-4 border-b border-blue-gray-50";
+              return (
+                <tr className={classes} key={board.id}>
+                  <th className={classes}>{idx + 1}</th>
+                  <td className={classes}>{board.name}</td>
+                  <td className={classes}>
+                    {
+                      educationVariants.find(
+                        (variant) => variant.id === board.mediumId
+                      ).name
                     }
-                    className="cursor-pointer hover:text-red-500"
-                  />{" "}
-                  <FaEdit
-                    onClick={() =>
-                      toast.success("Curriculum Board updated successfully")
-                    }
-                    className="cursor-pointer hover:text-sky-500"
-                  />
-                </td>
-              </tr>
-            ))}
+                  </td>
+                  <td className={`${classes} w-[120px]`}>
+                    <div className="flex justify-evenly items-center">
+                      <FaTrash
+                        onClick={() =>
+                          toast.success("College deleted successfully")
+                        }
+                        className="cursor-pointer hover:text-red-500"
+                      />{" "}
+                      <FaEdit
+                        onClick={() =>
+                          toast.success("College updated successfully")
+                        }
+                        className="cursor-pointer hover:text-sky-500"
+                      />
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
