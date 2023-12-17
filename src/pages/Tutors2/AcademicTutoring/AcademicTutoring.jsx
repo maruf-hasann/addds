8583497d8bdb4a63de9e9dc2de8b43e7a9c926d3
Murad501 <mm845635@gmail.com>
2023-12-17 21:10:05 +0600

    [Done] create a modal for show large text

--- src/pages/Tutors2/AcademicTutoring/AcademicTutoring.jsx
+++ src/pages/Tutors2/AcademicTutoring/AcademicTutoring.jsx
@@ -1,14 +1,24 @@
 import { FaStreetView } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import { useGetTutorInfoFilterDataQuery } from "../../../store/service/tutorInfoFilter/tutorInfoFilterApiService";
+import { Dialog, DialogBody } from "@material-tailwind/react";
+import { CiCircleRemove } from "react-icons/ci";
+import { useState } from "react";
 
 const AcademicTutoring = () => {
+  const [open, setOpen] = useState(false);
+  const [textForModal, setTextForModal] = useState(null);
+
+  const handleOpen = () => {
+    setOpen(!open);
+    setTextForModal(null);
+  };
   const { data: tutorsInfoData } =
     useGetTutorInfoFilterDataQuery("8801708666342");
   const tutorsInfo = tutorsInfoData?.data[0];
-  console.log(tutorsInfo);
 
-  const tableDataClasses = "p-4 border-b border-blue-gray-50 whitespace-nowrap";
+  const tableDataClasses =
+    "px-4 py-3 border-b border-blue-gray-50 whitespace-nowrap";
   const tableDataArrayClasses =
     "bg-gray-200 mx-1 px-2 py-1 hover:text-[#1C6AAF] hover:bg-light-blue-50";
   const tableHeadClasses =
@@ -72,13 +82,7 @@ const AcademicTutoring = () => {
               <th className={tableHeadClasses}>Max. Exp. Salary</th>
               <th className={tableHeadClasses}>Tutoring Location</th>
               <th className={tableHeadClasses}>Personal Statement</th>
-              <th className={`${tableHeadClasses} text-center`}>
-                <h2>Promotion</h2>
-                <div className="flex justify-between border-t-2 border-white">
-                  <p className="border-r border-white w-full">Image</p>
-                  <p className="border-l border-white w-full">Video</p>
-                </div>
-              </th>
+              <th className={`${tableHeadClasses} text-center`}>Promotion</th>
               <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4 w-[120px] text-center">
                 Actions
               </th>
@@ -214,7 +218,30 @@ const AcademicTutoring = () => {
                   {tutorsInfo?.additionalInfo?.yearsOfExperience}
                 </td>
                 <td className={tableDataClasses}>
-                  {tutorsInfo?.additionalInfo?.teachingHistory}
+                  <div>
+                    {tutorsInfo?.additionalInfo?.teachingHistory?.length >
+                    30 ? (
+                      <p>
+                        {tutorsInfo?.additionalInfo?.teachingHistory?.slice(
+                          0,
+                          30
+                        )}
+                        <span
+                          onClick={() => {
+                            setOpen(!open);
+                            setTextForModal(
+                              tutorsInfo?.additionalInfo?.teachingHistory
+                            );
+                          }}
+                          className="cursor-pointer text-[#1D6AAF]"
+                        >
+                          ...See More
+                        </span>
+                      </p>
+                    ) : (
+                      tutorsInfo?.additionalInfo?.teachingHistory
+                    )}
+                  </div>
                 </td>
                 <td className={tableDataClasses}>
                   {tutorsInfo?.additionalInfo?.tutoringPlace?.map(
@@ -240,7 +267,6 @@ const AcademicTutoring = () => {
                 <td className={tableDataClasses}>
                   {tutorsInfo?.additionalInfo?.maxExpectedSalary}
                 </td>
-
                 <td className={tableDataClasses}>
                   {tutorsInfo?.additionalInfo?.tutoringLocation?.map(
                     (location, idx) => (
@@ -251,15 +277,41 @@ const AcademicTutoring = () => {
                   )}
                 </td>
                 <td className={tableDataClasses}>
-                  {tutorsInfo?.additionalInfo?.personalStatement}
+                  <div>
+                    {tutorsInfo?.additionalInfo?.personalStatement?.length >
+                    30 ? (
+                      <p>
+                        {tutorsInfo?.additionalInfo?.personalStatement?.slice(
+                          0,
+                          30
+                        )}
+                        <span
+                          onClick={() => {
+                            setOpen(!open);
+                            setTextForModal(
+                              tutorsInfo?.additionalInfo?.personalStatement
+                            );
+                          }}
+                          className="cursor-pointer text-[#1D6AAF]"
+                        >
+                          ...See More
+                        </span>
+                      </p>
+                    ) : (
+                      tutorsInfo?.additionalInfo?.personalStatement
+                    )}
+                  </div>
                 </td>
                 <td className={tableDataClasses}>
                   <div className="flex justify-between">
-                    <p className="border-r  w-full">Image</p>
-                    <p className="border-l  w-full">Video</p>
+                    <p className="border-r  w-full px-5 cursor-pointer hover:text-[#1D6AAF]">
+                      Show Images
+                    </p>
+                    <p className="border-l  w-full px-5 cursor-pointer hover:text-[#1D6AAF]">
+                      Show Videos
+                    </p>
                   </div>
                 </td>
-
                 <td
                   className={`${tableDataClasses} w-[120px] flex gap-3 border-b-0`}
                 >
@@ -278,6 +330,20 @@ const AcademicTutoring = () => {
           </tbody>
         </table>
       </div>
+
+      {textForModal && open ? (
+        <Dialog open={open} handler={handleOpen}>
+          <DialogBody className="relative p-5 pr-10">
+            <CiCircleRemove
+              onClick={handleOpen}
+              className="cursor-pointer text-4xl text-red-500 absolute top-0 right-0"
+            />
+            {textForModal}
+          </DialogBody>
+        </Dialog>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
