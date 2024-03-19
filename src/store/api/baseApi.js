8ccb92932f21faa3c6af0e9e91c8c6da18e0bc38
its-kayes <kayes.ek8@gmail.com>
2024-03-19 17:12:03 +0600

    change base api. dev->api

--- src/store/api/baseApi.js
+++ src/store/api/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `https://dev.schooling.app/api/v1/`,
+    baseUrl: `https://api.schooling.app/api/v1/`,
   }),
   endpoints: () => ({}),
 });
